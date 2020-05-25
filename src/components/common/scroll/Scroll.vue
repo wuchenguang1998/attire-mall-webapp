b6c81629c2ca171bc9eb4381e237cd12b3df90b1
wuchenguang1998 <wuchenguang1998@sina.com>
2020-05-25 23:49:19 +0800

    second commit

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+  export default {
+    props:{
+      probeType:{
+        type:Number,
+        default:0
+      }
+    },
+    data(){
+      return{
+        scroll:null
+      }
+    },
+    mounted(){
+      //1.创建BScroll对象
+      this.scroll=new BScroll(this.$refs.wrapper,{
+        probeType:this.probeType,
+        click:true
+      })
+      //2.监听滚动的位置
+      this.scroll.on('scroll',position=>{
+        this.$emit('scroll',position)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,61 +3,26 @@
     <nav-bar class="home-nav">
       <div slot="center">穿搭show</div>
     </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommend="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
+    <scroll class="content" ref="scroll" :probe-type="3" @scroll="contentscroll">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommend="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick"></tab-control>
+      <good-list :goods="showGoods"></good-list>
+    </scroll>
+    <back-top v-show="isShow" @click.native="backClick"></back-top>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import HomeSwiper from './childComps/HomeSwiper';
-import RecommendView from './childComps/RecommendView';
-import FeatureView from './childComps/FeatureView'
-import TabControl from 'components/content/tabControl/TabControl'
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodsList";
+import Scroll from "components/common/scroll/Scroll";
+import BackTop from "components/content/backTop/BackTop"
 import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
@@ -67,52 +32,90 @@ export default {
     HomeSwiper,
     RecommendView,
     FeatureView,
-    TabControl
+    TabControl,
+    GoodList,
+    Scroll,
+    BackTop
   },
   data() {
     return {
-      banners:[],
-      recommends:[],
-      goods:{
-        'pop':{page:0,list:[]},
-        'new':{page:0,list:[]},
-        'sell':{page:0,list:[]},
-      }
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop",
+      isShow:false
+    };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     }
   },
-  created() {   //created中只写主要逻辑
+  created() {
+    //created中只写主要逻辑
     //1.请求多个数据
-    this.getHomeMultidata()
+    this.getHomeMultidata();
     //2.请求商品数据
-    this.getHomeGoods('pop')
-    this.getHomeGoods('new')
-    this.getHomeGoods('sell')
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
-  methods:{
-    getHomeMultidata(){
+  methods: {
+    //网络请求相关方法
+    getHomeMultidata() {
       getHomeMultidata().then(res => {
-      this.banners=res.data.banner.list
-      this.recommends=res.data.recommend.list
-    })
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
     },
-    getHomeGoods(type){
-      const page=this.goods[type].page+1
-      getHomeGoods(type,page).then(res=>{
-        this.goods[type].list.push(...res.data.list)  //push中可以使用扩展运算符
-        this.goods[type].page+=1
-      })
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list); //push中可以使用扩展运算符
+        this.goods[type].page += 1;
+      });
+    },
+    //事件句柄
+    tabClick(index) {
+      // console.log(index)
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    backClick(){
+      //操作scroll组件内部的scroll变量,使其返回顶部
+      this.$refs.scroll.scroll.scrollTo(0,0,300)
+    },
+    contentscroll(position){
+      if(position.y<-1000){
+        this.isShow=true
+      }else{
+        this.isShow=false
+      }
+      //或者直接写成this.isShow=(-position.y)>1000
     }
   }
 };
 </script>
 
 <style scoped>
-#home{
+#home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
-/* #home{
-  margin-bottom: 49px;
-} */
 .home-nav {
   position: fixed;
   left: 0;
@@ -122,8 +125,23 @@ export default {
   color: white;
   z-index: 100;
 }
-.tab-control{
+.tab-control {
   position: sticky;
-  top: 44px;   /* 黏连属性必须设置top */
+  top: 44px;     /* 黏连属性必须设置top */
+  z-index: 100;
+}
+/* 运算符之间要预留问题，不然会出问题 */
+/* .content{
+  height: calc(100% - 93px);
+  overflow: hidden;
+  margin-top: 44px;
+} */
+.content{
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 44px;
+  left: 0;
+  right: 0;
 }
 </style>
